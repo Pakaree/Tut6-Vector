e92e8bb30af48a393323d2830fe732a4ed257482
Pakaree <jpakaree@gmail.com>
2016-04-09 23:12:08 +0200

    function template definition

---
+++ tut6/tut6/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+// function template definition
+template <typename T>
+void sort(const T*const array, int count)
+{
+	for (int i = 0; i < count-1; i++)
+		for (int j = i+1; j = count; j++)
+			tmp = array[j];
+			array[j] = array[i];
+			array[i] = tmp;
+
+			cout << array[i] << " ";
+			cout << endl;
+
+}
+int main
